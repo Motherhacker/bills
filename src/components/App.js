539ad4e510e419393900d853cbc2486744b5c4ff
Attila Repassy <attila.repassy@gmail.com>
2020-12-19 18:34:14 +0100

    google auth login added

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Content from "./Content";
+import history from "../history";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={Content} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
